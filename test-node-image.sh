a735cf8e05fe9f4cc8b9a0ad3e30a18d7652c8a3
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2022-06-22 17:33:21 +0530

    Update test-node-image.sh

--- test-node-image.sh
+++ test-node-image.sh
@@ -1,5 +1,5 @@
 echo -e "Install Docker\n"
-mkdir -p ~/.go_workspace/src/sigs.k8s.io/kind
+sudo mkdir -p ~/.go_workspace/src/sigs.k8s.io/kind
 cd ~/.go_workspace/src/sigs.k8s.io/kind
 git clone https://github.com/kubernetes-sigs/kind.git .
 echo -e "Build Kind binary from the source code present in the master\n" 
@@ -21,10 +21,10 @@ kubectl get nodes -o wide
 kubectl get pods --all-namespaces -o wide
 kubectl get services --all-namespaces -o wide
 echo -e "Export logs\n"
-mkdir -p /tmp/kind
+sudo mkdir -p /tmp/kind
 sudo ./bin/kind export logs /tmp/kind
 echo -e "Publish Docker Image to Docker Hub\n"
-echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+echo "$DOCKERHUB_PASS" | sudo docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 sudo docker push mwaghmodepersistent/kindnode:latest
 export KUBE_VERSION=$(git -C /tmp/kubernetes log -1 --pretty='%h')
 export KIND_VERSION=$(git log -1 --pretty='%h')
