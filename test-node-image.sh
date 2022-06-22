004f22d645a8e6ec6d057094be46fbffce77b0da
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2022-06-22 23:00:29 +0530

    Update test-node-image.sh

--- test-node-image.sh
+++ test-node-image.sh
@@ -1,8 +1,9 @@
-rm -rf ~/.go_workspace/src/sigs.k8s.io/
+sudo rm -rf ~/.go_workspace/src/sigs.k8s.io/
 sudo mkdir -p  ~/.go_workspace/src/sigs.k8s.io/
 cd ~/.go_workspace/src/sigs.k8s.io/
 git clone https://github.com/kubernetes-sigs/kind.git
 cd kind
 sudo make build
+sudo rm -rf /tmp/kubernetes
 git clone https://github.com/kubernetes/kubernetes.git /tmp/kubernetes
 sudo ./bin/kind build node-image --image mwaghmodepersistent/kindnode:amd64 --kube-root /tmp/kubernetes -v=3
