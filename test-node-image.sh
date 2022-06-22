1708b6e09a17235a9641dc4dd254ba76a7402abb
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2022-06-22 17:12:22 +0530

    Update test-node-image.sh

--- test-node-image.sh
+++ test-node-image.sh
@@ -1,7 +1,6 @@
 echo -e "Install Docker\n"
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2 tc
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli
+sudo apt-get install -y yum-utils device-mapper-persistent-data lvm2 tc
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 systemctl daemon-reload
 systemctl restart docker
 sudo systemctl enable docker
@@ -10,7 +9,7 @@ cd ~/.go_workspace/src/sigs.k8s.io/kind
 git clone https://github.com/kubernetes-sigs/kind.git .
 echo -e "Build Kind binary from the source code present in the master\n" 
 echo -e "Install make\n" 
-yum install make
+apt-get install make
 make build
 git clone https://github.com/kubernetes/kubernetes.git /tmp/kubernetes
 echo -e "Build node image\n" 
