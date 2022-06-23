d86e5ebef8c5e4e9d396515a90e3d69d62e183b5
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2022-06-23 12:33:16 +0530

    Update test-node-image.sh

--- test-node-image.sh
+++ test-node-image.sh
@@ -1,15 +1,18 @@
-sudo rm -rf ~/.go_workspace/src/sigs.k8s.io/kind
-sudo rm -rf /tmp/kubernetes
-sudo mkdir -p ~/.go_workspace/src/sigs.k8s.io/kind
-sudo chown ubuntu:ubuntu ~/.go_workspace/src/sigs.k8s.io/kind
-cd ~/.go_workspace/src/sigs.k8s.io/kind
+sudo rm -rf go
+sudo wget https://go.dev/dl/go1.18.3.linux-ppc64le.tar.gz
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.3.linux-ppc64le.tar.gz
+sudo export PATH=$PATH:/usr/local/go/bin
+sudo mkdir -p go/src/k8s.io
+cd go/src/k8s.io
+git clone https://github.com/kubernetes/kubernetes
+
+
+
 echo -e "Download kind source code\n" 
 git clone https://github.com/kubernetes-sigs/kind.git .
 echo -e "Build Kind binary from the source code present in the master\n" 
 make build
 echo -e "Check the kind version\n"
 ./bin/kind version
-echo -e "Download kubernetes source code\n" 
-git clone https://github.com/kubernetes/kubernetes.git /tmp/kubernetes
 echo -e "Build node image for ppc64le architecture\n" 
-sudo ./bin/kind build node-image --image mwaghmodepersistent/kindnode:amd64 --kube-root /tmp/kubernetes -v=3
+sudo ./bin/kind build node-image --image mwaghmodepersistent/kindnode:amd64 --kube-root go/src/k8s.io/kubernetes -v=3
