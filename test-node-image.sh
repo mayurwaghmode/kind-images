f96b1c3eda3cd411f740faa0e9febde5160b70c3
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2022-06-23 10:51:09 +0530

    Update test-node-image.sh

--- test-node-image.sh
+++ test-node-image.sh
@@ -3,7 +3,12 @@ sudo rm -rf /tmp/kubernetes
 sudo mkdir -p ~/.go_workspace/src/sigs.k8s.io/kind
 sudo chown ubuntu:ubuntu ~/.go_workspace/src/sigs.k8s.io/kind
 cd ~/.go_workspace/src/sigs.k8s.io/kind
+echo -e "Download kind source code\n" 
 git clone https://github.com/kubernetes-sigs/kind.git .
+echo -e "Build Kind binary from the source code present in the master\n" 
 make build
+echo -e "Check the kind version\n"
+./bin/kind version
+echo -e "Download kubernetes source code\n" 
 git clone https://github.com/kubernetes/kubernetes.git /tmp/kubernetes
 sudo ./bin/kind build node-image --image mwaghmodepersistent/kindnode:amd64 --kube-root /tmp/kubernetes -v=3
