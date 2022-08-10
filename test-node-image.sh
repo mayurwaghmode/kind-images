8b3eb8e2ec8ab8a1efe850102622ffff62e7bda6
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2022-08-10 15:59:02 +0530

    Update test-node-image.sh

--- test-node-image.sh
+++ test-node-image.sh
@@ -1,3 +1,11 @@
+if (( $# == 4 ))
+then
+    GH_REPO=$1
+    QUAY_REPO=$2
+	  QUAY_USER=$3
+    QUAY_PASS=$4
+fi
+
 sudo git clone -b hosted-multi-arch https://github.com/mayurwaghmode/circleCI-multi-arch.git /root/"$GH_REPO"
 sudo cd /root/"$GH_REPO"
 sudo echo "Hello"
